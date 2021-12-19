f96c8f76e507175146f446b23a835e2aeb1efcc0
sunyixin <43490884+Cokebear7@users.noreply.github.com>
2021-12-19 22:40:38 +0800

    use redux middleware to implement async action

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,9 @@ import 'antd/dist/antd.css';
 import "./i18n/configs"
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import axios from "axios";
+
+axios.defaults.headers['x-icode'] = '443FD0A2DD98CDBE';
 
 ReactDOM.render(
   <React.StrictMode>
